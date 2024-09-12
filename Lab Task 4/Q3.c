a4d1fea9c1d1ab1ef1bf0216b6203f8bd963b613
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-09-13 00:22:18 +0500

    Q3 of Lab Task 4

---
+++ Lab Task 4/Q3.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() 
+{
+    int num1, num2, result;
+    char operator;
+
+  
+    printf("Enter your first number: ");
+    scanf("%d", &num1);
+
+  
+    printf("Enter your second number: ");
+    scanf("%d", &num2);
+
+    printf("Enter the type of operator to be used (+, -, *, /): ");
+    scanf(" %c", &operator);   
+    switch (operator) {
+        case '+':
+            result = num1 + num2;
+            printf("The result is %d\n", result);
+            break;
+        
+        case '-':
+            result = num1 - num2;
+            printf("The result is %d\n", result);
+            break;
+        
+        case '*':
+            result = num1 * num2;
+            printf("The result is %d\n", result);
+            break;
+        
+        case '/':
+            if (num2 == 0) 
+                {printf("Math Error: Division by zero is not allowed.\n");}
+            else
+                {result = num1 / num2;
+                printf("The result is %d\n", result);}
+            break;
+
+        default:
+            printf("Invalid Operator\n");
+            break;
+    }
+
+    return 0;
+}
