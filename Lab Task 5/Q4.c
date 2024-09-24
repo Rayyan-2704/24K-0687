ad057adbe7ed20bab7906d504cede54ebfb7bdb4
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-09-25 03:05:37 +0500

    Q4 of lab task 5

---
+++ Lab Task 5/Q4.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{ 
+ int ph;
+ printf("Enter the pH value: ");
+ scanf("%d", &ph);
+ 
+ if (ph > 7)
+    {if (ph < 12)
+        {printf("Alkaline");}
+     else
+        {printf("Very Alkaline");}
+    }
+ else if (ph == 7)
+    {printf("Neutral");}
+ else if (ph > 2)
+    {printf("Acidic");}
+ else
+    {printf("Very Acidic");}
+
+ return 0;
+}
