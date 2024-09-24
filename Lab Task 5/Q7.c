b7f8ddc81959574d1a10b5347ff1cd8dfe6bc761
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-09-25 03:50:08 +0500

    Q7 of lab task 5

---
+++ Lab Task 5/Q7.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{ 
+ int num;
+ printf("Enter a single integer number: ");
+ scanf("%d", &num);
+ (num % 10 == 0) ? printf("Last digit is zero") :printf("Last digit is non-zero");
+ return 0;
+}
