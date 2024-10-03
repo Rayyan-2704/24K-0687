f0cf560271790ab8622b5ac8a87ff8e58128da21
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-10-03 15:07:38 +0500

    Q4 of Lab Task 6

---
+++ Lab Task 6/Q4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{ 
+ int num, digit, calc=0, new_num;
+ printf("Enter a number: ");
+ scanf("%d", &num);
+ new_num=num;
+ while(new_num>0)
+ {digit = new_num % 10;
+  new_num = new_num / 10;
+  calc=calc + (digit*digit*digit);}
+ if(calc == num)
+   {printf("The number entered is an Armstrong number.");}
+ else 
+   {printf("The number entered is not an Armstrong number.");}
+ return 0;
+}
