3fc538edd5a042f6930b68ee3559abe5b88740d2
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-10-03 21:28:54 +0500

    Q4 of Lab Task 6

--- Lab Task 6/Q4.c
+++ Lab Task 6/Q4.c
@@ -4,6 +4,7 @@ int main()
  int num, digit, calc=0, new_num;
  printf("Enter a number: ");
  scanf("%d", &num);
+ // new_num is a variable here being used to perform calculations with the value of number without actually changing that value in the num variable
  new_num=num;
  while(new_num>0)
  {digit = new_num % 10;
