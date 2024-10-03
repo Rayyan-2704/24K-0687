d38c53903efa20e090df11fdbd674183b3f54f44
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-10-03 23:08:43 +0500

    Q2 of Lab Task 6

---
+++ Lab Task 6/Q2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+  int num, i, prime = 1;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+  
+  if(num == 0 || num == 1)
+    {printf("The number entered is not a prime number.");}
+  else
+    {for(i=2 ; i<num ; i++)
+      {if(num % i == 0)
+         {prime = 0;
+          break;} 
+       else
+         {continue;} }
+    if(prime == 1)
+      {printf("The number entered is a prime number.");}
+    else if(prime == 0)
+      {printf("The number entered is not a prime number.");} }
+  
+  return 0;
+}
+
+    
