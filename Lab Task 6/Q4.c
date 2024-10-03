dc2e3d6420d257ad32e2ceb5d6cbac19b14dfae4
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-10-03 21:38:23 +0500

    Q4 of Lab Task 6

--- Lab Task 6/Q4.c
+++ Lab Task 6/Q4.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 { 
- int num, digit, calc=0, new_num;
+ int num, digit, calc=0, temp;
  printf("Enter a number: ");
  scanf("%d", &num);
  // new_num is a variable here being used to perform calculations with the value of number without actually changing that value in the num variable
- new_num = num;
- while(new_num > 0)
- {digit = new_num % 10;
-  new_num = new_num / 10;
+ temp = num;
+ while(temp > 0)
+ {digit = temp % 10;
+  temp = temp / 10;
   calc = calc + (digit*digit*digit);}
  if(calc == num)
    {printf("The number entered is an Armstrong number.");}
