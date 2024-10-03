a0eb6136499c38b2f1a6c3cd62de94cb680231ba
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-10-03 21:47:05 +0500

    Q6 of Lab Task 6

--- Lab Task 6/Q6.c
+++ Lab Task 6/Q6.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main()
-{ int i=0;
+{ 
+  int i=0;
   do
   {if(i == 0 || i == 5)
      {printf("*       *\n");}
    else
      {printf("* * * * *\n");}
-   i++;}
-  while(i<=5);
+   i++; }
+  while(i <= 5);
   return 0;
 } 
