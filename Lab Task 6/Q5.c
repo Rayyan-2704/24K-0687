d4f8b0846c2a1b1cc910089fb84a6236bfdaf2ba
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-10-13 13:34:56 +0500

    Update Q5.c

--- Lab Task 6/Q5.c
+++ Lab Task 6/Q5.c
@@ -9,3 +9,21 @@ int main()
      {printf("*       *\n");} }
   return 0;
 }
+
+// A better approach to this problem would be as follows:
+#include<stdio.h>
+int main()
+{
+  int i;
+  for(i=1 ; i<=30 ; i++)
+    {if(i<=5 || i>=26)
+        {printf("* ");}
+     else if((i>5 || i<26) && (i%5==1 || i%5==0))
+        {printf("* ");}
+     else 
+        {printf("  ");}
+     
+     if (i%5 == 0)
+        {printf("\n");} }
+        return 0;
+}
