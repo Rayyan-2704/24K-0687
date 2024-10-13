b57b5b04deeed4f9d560eb9ef1be8d45fd5eb121
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-10-13 14:01:34 +0500

    Update Q6.c

--- Lab Task 6/Q6.c
+++ Lab Task 6/Q6.c
@@ -10,4 +10,24 @@ int main()
    i++; }
   while(i <= 5);
   return 0;
-} 
+}
+
+// A better approach to this problem would be as follows:
+#include<stdio.h>
+int main()
+{
+  int i=1;
+  do
+  {if(i>5 && i<26)
+     {printf("* ");}
+   else if((i<=5 || i>=26) && (i%5==1 || i%5==0))
+     {printf("* ");}
+   else 
+     {printf("  ");}
+     
+   if(i%5 == 0)
+     {printf("\n");}
+   i++;}
+  while(i<=30);
+  return 0;
+}
