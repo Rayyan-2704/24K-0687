6cf5ca80d87cd30d12a8fb466f442f8cbb764813
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-10-16 16:34:22 +0500

    Update Q5.c

--- Lab Task 6/Q5.c
+++ Lab Task 6/Q5.c
@@ -18,7 +18,7 @@ int main()
   for(i=1 ; i<=30 ; i++)
     {if(i<=5 || i>=26)
         {printf("* ");}
-     else if((i>5 || i<26) && (i%5==1 || i%5==0))
+     else if((i>5 && i<26) && (i%5==1 || i%5==0))
         {printf("* ");}
      else 
         {printf("  ");}
