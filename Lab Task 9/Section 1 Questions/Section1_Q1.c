598feb696c1fdaa2f16473db6fce9cddd2114792
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-11-01 21:29:39 +0500

    Add files via upload

---
+++ Lab Task 9/Section 1 Questions/Section1_Q1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+float product(float a, float b);
+
+int main(){
+    float num1, num2;
+    printf("Enter any 2 numbers: ");
+    scanf("%f %f", &num1, &num2);
+    float result=product(num1,num2);
+    printf("The product of the 2 numbers is %.2f", result);
+    return 0;
+}
+
+float product(float a, float b){
+    float c = a * b;
+    return c;
+ }
