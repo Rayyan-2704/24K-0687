dd0f085d465d148023687596b2334f1dfec70eae
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-11-14 19:45:26 +0500

    Add files via upload

---
+++ Lab Task 10/Section 1 Questions/Section1_Q1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int sum(int num);
+
+int main()
+{
+    int num;
+    printf("Enter the integer: ");
+    scanf("%d", &num);
+
+    printf("The sum of digits of %d is %d.", num, sum(num));
+}
+
+int sum(int num)
+{
+    int digit;
+    if (num > 0)
+    {
+        digit = num % 10;
+        return digit + sum(num / 10);
+    }
+    else
+    {
+        return 0;
+    }
+}
