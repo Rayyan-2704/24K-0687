18debd347e905195559b6a7d4d0cc0fbdefd9edd
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-11-24 17:29:47 +0500

    Add files via upload

---
+++ PF Assignment 3/Q3.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int validateEmail(char *email);
+
+int main()
+{
+    int length = 50;
+    char *email;
+    email = (char *)malloc(length * sizeof(char));
+
+    if (email == NULL)
+    {
+        printf("Error! Memory not allocated.");
+        return 1;
+    }
+
+    printf("Enter your email address: ");
+    fgets(email, length, stdin);
+    email[strcspn(email, "\n")] = '\0';
+
+    if (validateEmail(email))
+    {
+        printf("Valid Email.");
+    }
+    else
+    {
+        printf("Invalid Email.");
+    }
+
+    free(email);
+    return 0;
+}
+
+int validateEmail(char *email)
+{
+    if (email == NULL || strlen(email) == 0 || strcmp(email, " ") == 0)
+    {
+        return 0;
+    }
+
+    int count_at = 0, count_dot = 0;
+    for (int i = 0; *(email + i) != '\0'; i++)
+    {
+        if (*(email + i) == '@')
+        {
+            count_at++;
+        }
+        else if (*(email + i) == '.' && count_at > 0)
+        {
+            count_dot++;
+        }
+    }
+
+    if (count_at == 1 && count_dot >= 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
