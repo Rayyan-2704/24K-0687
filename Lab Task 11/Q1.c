92cd9f6fa3c22b7d3c431d6a9fc3510c407d309e
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-11-25 00:49:16 +0500

    Add files via upload

---
+++ Lab Task 11/Q1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    FILE *fptr1, *fptr2;
+    char stri[100];
+
+    fptr1 = fopen("sample.txt", "w");
+    fprintf(fptr1, "Hello World, my name is Rayyan Aamir!");
+    fclose(fptr1);
+
+    fptr1 = fopen("sample.txt", "r");
+    if (fptr1 == NULL)
+    {
+        printf("Error in opening the file.");
+        return 1;
+    }
+    fptr2 = fopen("copy.txt", "w");
+    fgets(stri, sizeof(stri), fptr1);
+    fprintf(fptr2, "%s", stri);
+    fclose(fptr2);
+    fclose(fptr1);
+
+    return 0;
+}
