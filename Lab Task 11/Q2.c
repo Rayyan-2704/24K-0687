33b2ea00df17fe893fb06225334b483f1c81f25c
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-11-26 20:05:37 +0500

    Update Q2.c

--- Lab Task 11/Q2.c
+++ Lab Task 11/Q2.c
@@ -1,7 +1,12 @@
 /* How would you implement a program to count the occurrences of each word in a text
 file? Write the approach and code. */
 
-/* Approach: The solution to this porblem was to first create a 2D char array that stores every word written in the text file. Then read each word till a space occurs and store it into a "word" char array. This word array is first compared with every string present in the 2D array to see if it is a repeated one. If true, the count of that word is increased and a flag is raised meaning this word is NOT a new word but a repeated one and the loop breaks. If the loops ends and the word is NOT a repeated one, a condition is to be checked outside the loop first requiring the flag to be down and the word count not exceeding the max limit, if true the new word is stored in the 2D array, its count is incremented to 1 and the total word count is also increased by 1. To further maintain consistency, all the words fetched from the file are converted to lowercase and the punctuations are removed if any. */
+/* Approach: The solution to this porblem was to first create a 2D char array that stores every word written in the text file. Then read each word till a 
+space occurs and store it into a "word" char array. This word array is first compared with every string present in the 2D array to see if it is a repeated one.
+If true, the count of that word is increased and a flag is raised meaning this word is NOT a new word but a repeated one and the loop breaks. If the loops ends
+and the word is NOT a repeated one, a condition is to be checked outside the loop first requiring the flag to be down and the word count not exceeding the max 
+limit, if true the new word is stored in the 2D array, its count is incremented to 1 and the total word count is also increased by 1. To further maintain 
+consistency, all the words fetched from the file are converted to lowercase and the punctuations are removed if any. */
 
 #include <stdio.h>
 #include <string.h>
