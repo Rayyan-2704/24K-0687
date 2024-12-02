5026bf451e918200d76d0df0c605ed79c775657d
Rayyan Aamir <aamir.rayyan27@gmail.com>
2024-12-02 23:21:56 +0500

    Add files via upload

---
+++ Lab Task 12/Q4.c
@@ -0,0 +1,41 @@
+/* Write a program that ask the user to enter the total 'N' no of characters in user's name {First
+Name + Last Name} to create a dynamic array of characters. After create a dynamic array of
+that 'N' no of characters using malloc or calloc function. Finally copy your full name in it that has
+already been taken from the user before
+Dynamic Array = "Muhib Ahmed"; */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(){
+    char buffer[50];
+    printf("Enter your first and last name together as a single string: ");
+    fgets(buffer, sizeof(buffer), stdin);
+    buffer[strcspn(buffer, "\n")] = '\0'; // Adding the null terminator at the end of the string
+    int length = strlen(buffer);
+    length++; // To count the space of the null terminator as well
+
+    int N;
+    printf("Enter the total number of characters of the full name entered: ");
+    scanf("%d", &N);
+
+    char *arr = (char *) calloc(N, sizeof(char));
+    if(arr == NULL){
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+
+    if(N >= length){
+        strcpy(arr, buffer);
+    }
+    else{
+        printf("The size of the array, which depends on the total number of characters entered, is insufficient to store the whole name.\n");
+        return 1;
+    }
+
+    printf("Dynamic array: %s", arr);
+
+    free(arr);
+    return 0;
+}
